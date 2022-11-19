d223d0862f4d47931b38327d770e8381e290890b
tarikozturk017 <75666245+tarikozturk017@users.noreply.github.com>
2022-11-19 13:44:20 -0500

    timer is set

--- speed-typing/src/App.js
+++ speed-typing/src/App.js
@@ -1,15 +1,39 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
 function App() {
-    return(
+    const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(5)
+    
+    function handleChange(e) {
+        const {value} = e.target
+        setText(value)
+    }
+    
+    function calculateWordCount(text) {
+        const wordsArr = text.trim().split(" ")
+        return wordsArr.filter(word => word !== "").length
+    }
+    
+    useEffect(() => {
+        if(timeRemaining > 0) {
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        }
+    }, [timeRemaining])
+    
+    return (
         <div>
-            <h1>h1</h1>
-            <textarea value="textarea"/>
-            <h4>h4</h4>
-            <button>Start</button>
-            <h1>another h1</h1>
+            <h1>How fast do you type?</h1>
+            <textarea
+                onChange={handleChange}
+                value={text}
+            />
+            <h4>Time remaining: {timeRemaining}</h4>
+            <button onClick={() => console.log(calculateWordCount(text))}>Start</button>
+            <h1>Word count: ???</h1>
         </div>
-    )   
+    )
 }
 
 export default App
